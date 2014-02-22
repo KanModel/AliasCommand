0246b90628d037f6ee6dcb1a015053128e46d43a
kgdwhsk <136995219@qq.com>
2014-02-22 09:06:29 +0800

    1.1版本发布

--- AliasCommand.php
+++ AliasCommand.php
@@ -3,7 +3,7 @@
 __PocketMine Plugin__
 name=AliasCommand
 description=Custom alias
-version=1.0
+version=1.1
 author=kgdwhsk
 class=AliasCommand
 apiversion=11,12
@@ -40,10 +40,72 @@ class AliasCommand implements Plugin{
 				{
 					if(isset($params[1]))
 					{
-						//$cm = array($params[0] => $params[1]);
-						$this->api->plugin->writeYAML($this->path."config.yml",array($params[0] => $params[1]));
+						if(count($params) === 2)
+						{
+							//$cm = array($params[0] => $params[1]);
+							$this->api->plugin->writeYAML($this->path."config.yml",array($params[0] => $params[1]));
+							$this->init();
+							//$len = count($params);
+							return "Set command ".$params[0]." with alias ".$params[1]." succeed !";
+						}
+						/*elseif(count($params) === 3)
+						{
+							$ma = $params[0]." ".$params[1];
+							$this->api->plugin->writeYAML($this->path."config.yml",array($ma => $params[2]));
+							$this->init();
+							return "Set command ".$ma." with alias ".$params[2]." succeed !";
+						}*/
+						else
+						{
+							$la = count($params) - 1;
+							$lb = $la - 1;
+							$par = $params[0]." ".$params[1];
+							$pa = "";
+							for ($q = 2;$q <= $lb;$q++)
+							{
+								$pab = " $params[$q]";
+								$pa .= $pab;
+							}
+							$par .= $pa;
+							$this->api->plugin->writeYAML($this->path."config.yml",array($par => $params[$la]));
+							$this->init();
+							return "Set command ".$par." with alias ".$params[$la]." succeed !";
+						}
+						/*$this->api->plugin->writeYAML($this->path."config.yml",array($params[0] => $params[1]));
 						$this->init();
-						return "Set command ".$params[0]." with alias ".$params[1]." succeed !";
+						$len = count($params);
+						return "Set command ".$params[0]." with alias ".$params[1]." succeed !".$len;
+						$len = count($params);
+						if($len = 2)
+						{
+							//$cm = array($params[0] => $params[1]);
+							$this->api->plugin->writeYAML($this->path."config.yml",array($params[0] => $params[1]));
+							$this->init();
+							//$len = count($params);
+							return "Set command ".$params[0]." with alias ".$params[1]." succeed !";
+						}
+						else
+						{
+						}
+						$len = count($params);
+						$la = $len - 1;
+						do
+						{
+							$ma =+ "$parmas[0]";
+						}while($la >= );
+						foreach($params as $l => $cm)
+						{
+							if($l = $la)
+							{
+								continue;
+							}
+								$ma =+ " ".$cm;
+							console("$ma");
+						}
+						console("1111".$ma."fad");
+						$this->api->plugin->writeYAML($this->path."config.yml",array($ma => $params[$la]));
+						$this->init();
+						return "Set command ".$ma." with alias ".$params[$len - 1]." succeed !".$len;*/
 					}
 					else
 					{
